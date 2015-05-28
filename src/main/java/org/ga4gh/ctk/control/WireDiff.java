d61c17b9544a5131f52da5f2fa0ee0b9b437ebdf
Wayne Stidolph <wayne@stidolph.com>
2015-05-27 18:13:01 -0700

    start wiring WireDiff into AvroJson (for collection).

--- src/main/java/org/ga4gh/ctk/control/WireDiff.java
+++ src/main/java/org/ga4gh/ctk/control/WireDiff.java
@@ -1,14 +1,112 @@
 package org.ga4gh.ctk.control;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.fge.jsonpatch.JsonPatch;
+import com.github.fge.jsonpatch.diff.JsonDiff;
+import org.slf4j.Logger;
+
+import java.io.IOException;
+
+import static org.slf4j.LoggerFactory.getLogger;
 
 /**
+ * Control and transfer of wire-difference measurement and results.
+ * <p>
  * Used to signal need for wire-format difference measurement
  * of the Avro-defined objects used in a given interaction
  * Created by Wayne Stidolph on 5/27/2015.
  */
 public class WireDiff {
-    JsonNode expDiff;
-    JsonNode refDiff;
-    JsonNode expJson;
+    final static Logger log = getLogger(WireDiff.class);
+
+    JsonPatch expDiff;
+    JsonPatch refDiff;
+
+    // keep as String from the test definition
+    String expJsonStr;
+    JsonNode expJsonNode;
+
+    // JSON from the outside word is just a String, no type assumptions
+    String refJsonStr;
+    JsonNode refJsonNode;
+
+    String actJsonStr;
+    JsonNode actJsonNode;
+
+    boolean shouldDoRefCompare = false;
+
+    public static WireDiff DO_NOTHING = new WireDiff() {
+    };
+
+
+    /**
+     * Gets difference between the actual (rcd) and expected JSON.
+     *
+     * If the expDiff has already been calculated, it is returned; if
+     * it hasn't been calculated, then then it is calculated and stored first.
+     *
+     * @return the exp diff
+     */
+    public JsonPatch getExpDiff() {
+        if (expDiff == null) {
+            expDiff = JsonDiff.asJsonPatch(getActJsonNode(), getExpJsonNode());
+        }
+        return expDiff;
+    }
+
+    public JsonPatch getRefDiff() {
+        if (refDiff == null) {
+            refDiff = JsonDiff.asJsonPatch(getActJsonNode(), getRefJsonNode());
+        }
+        return refDiff;
+    }
+
+    public JsonNode getRefJsonNode() {
+        if (refJsonStr == null) {
+            // TODO get reference server json
+            // didn't arrive yet, block here until refJson available
+            // or times out and returns null
+            log.warn("NO REF STRING TO COMPARE");
+        }
+        refJsonNode = makeJsonNode(refJsonStr);
+        return refJsonNode;
+    }
+
+    public boolean isShouldDoRefCompare() {
+        return shouldDoRefCompare;
+    }
+
+    public void setShouldDoRefCompare(boolean shouldDoRefCompare) {
+        this.shouldDoRefCompare = shouldDoRefCompare;
+    }
+
+    public void setActJson(String theJson) {
+        actJsonStr = theJson;
+    }
+
+    public JsonNode getExpJsonNode() {
+        if (expJsonNode == null) {
+            expJsonNode = makeJsonNode(expJsonStr);
+        }
+        return expJsonNode;
+    }
+
+    public JsonNode getActJsonNode() {
+        if (actJsonNode == null) {
+            actJsonNode = makeJsonNode(actJsonStr);
+        }
+        return actJsonNode;
+    }
+
+    private JsonNode makeJsonNode(String jsonStr) {
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode rtnNode = null;
+        try {
+            rtnNode = mapper.readTree(jsonStr);
+        } catch (IOException e) {
+            log.warn("could not make JsonNode from string " + jsonStr, e);
+        }
+        return rtnNode;
+    }
 }
--- src/main/java/org/ga4gh/transport/AvroJson.java
+++ src/main/java/org/ga4gh/transport/AvroJson.java
@@ -20,6 +20,7 @@ import org.apache.avro.generic.GenericRecord;
 import org.apache.avro.io.*;
 import org.apache.avro.specific.SpecificDatumWriter;
 import org.apache.http.HttpStatus;
+import org.ga4gh.ctk.control.WireDiff;
 
 import java.io.*;
 
@@ -29,50 +30,79 @@ import static org.slf4j.LoggerFactory.getLogger;
  * Provide Avro/Json communications layer specific to GA4GH and with extensive logging in support of CTK use.
  * Created by Wayne Stidolph on 5/22/2015.
  */
-public class AvroJson<Q extends org.apache.avro.generic.GenericContainer,P extends org.apache.avro.generic.GenericContainer> {
+public class AvroJson<Q extends org.apache.avro.generic.GenericContainer, P extends org.apache.avro.generic.GenericContainer> {
     private static org.slf4j.Logger log;
     private static Table<String, String, Integer> messages;
 
-    /*
-     * table cells are | request (class, post/get, body/id) | response class  | response status code|
-     */
-    public static Table<String, String, Integer> getMessages() { return messages;}
     static {
         log = getLogger(AvroJson.class);
         messages = HashBasedTable.create();
     }
 
-    public enum DESER_MODE {
-        JACKSON_RELAXED,
-        JACKSON_AVRO,
-        AVRO_DIRECT
-    }
-
-    /*
+    final DatumWriter<Q> dw;
+    final Q theAvroReq;
+    /**
      * url root to system-under-test; e.g., "http://localhost:8000/v0.5.1/"
      */
     String urlRoot;
-    /*
+    /**
      * url root to live comparison server
      */
     String refRoot;
-    /*
+    /**
      * if true, duplicate request to refserver and compare results
      */
     boolean compareToRef = false;
 
     String path;
-    final DatumWriter<Q> dw;
-    final Q theAvroReq;
-    private P theResp;
-
     Schema reqSchema;
     Schema respSchema;
-
     ByteArrayOutputStream jsonBytes;
     HttpResponse<JsonNode> httpResp;
-
     DESER_MODE avroDeserializer = DESER_MODE.JACKSON_RELAXED; // default
+    private P theResp;
+    private WireDiff wireDiff;
+
+    /**
+     * Construct an AvroJson for a particular request/response interaction.
+     * The req and resp types parameterize this interaction object.
+     *
+     * @param req an instance of the avro *Request method object
+     * @param resp an instance of the avro *Response method object
+     * @param urlRoot String the server base (often includes a version number)
+     * @param path String the request target path as identified in the avdl
+     */
+    public AvroJson(Q req, P resp, String urlRoot, String path) {
+        this.theAvroReq = req;
+        this.theResp = resp;
+        this.dw = new SpecificDatumWriter<Q>();
+        this.urlRoot = urlRoot;
+        this.path = path;
+        this.wireDiff = null;
+    }
+
+    /**
+     * Construct an AvroJson for a particular request/response interaction.
+     * The req and resp types parameterize this interaction object.
+     *
+     * @param req an instance of the avro *Request method object
+     * @param resp an instance of the avro *Response method object
+     * @param urlRoot String the server base (often includes a version number)
+     * @param path String the request target path as identified in the avdl
+     * @param wireDiff {@code WireDiff} control and transfer of on-the-wire difference measure
+     */
+    public AvroJson(Q req, P resp, String urlRoot, String path, WireDiff wireDiff) {
+        this(req, resp, urlRoot, path);
+        this.wireDiff = wireDiff;
+    }
+
+    /*
+     * table cells are | request (class, post/get, body/id) | response class  | response status code|
+     */
+    public static Table<String, String, Integer> getMessages() {
+        return messages;
+    }
+
     public DESER_MODE getAvroDeserializer() {
         return avroDeserializer;
     }
@@ -80,81 +110,110 @@ public class AvroJson<Q extends org.apache.avro.generic.GenericContainer,P exten
     public void setAvroDeserializer(DESER_MODE deserialization_mode) {
         this.avroDeserializer = deserialization_mode;
     }
-    /*
-     * Construct an AvroJson for a particular request/response pattern
-     * @param req <Q> an instance of the avro *Request method object
-     * @param resp <P> an instance of the avro *Response method object
-     * @param urlRoot String the server base (often includes a version number)
-     * @param path String the request target path as identified in the avdl
+
+    /**
+     * Getter for the WireDiff (if present, triggers JSON collection).
+     *
+     * @return the {@code WireDiff}
      */
-    public AvroJson(Q req, P resp, String urlRoot, String path){
-        this.theAvroReq = req;
-        this.theResp = resp;
-        this.dw = new SpecificDatumWriter<Q>();
-        this.urlRoot = urlRoot;
-        this.path = path;
+    public WireDiff getWireDiff() {
+        return wireDiff;
     }
 
-    public P doPostResp(){
+    /**
+     * Setter for the WireDiff (if present, tiggers JSON collection).
+     *
+     * @param wireDiff
+     */
+    public void setWireDiff(WireDiff wireDiff) {
+        this.wireDiff = wireDiff;
+    }
+
+    /**
+     * Perform POST (according the data stored in this object at construction).
+     * <p>
+     * If this object has a WireDiff then the return JSON (if any) is copied into that.
+     * This method also tracks all message types sent and received, in the 'messages' Table.
+     *
+     * @return an instance of the response type (as set during onbject construction), can be null.
+     */
+    public P doPostResp() {
         reqSchema = theAvroReq.getSchema();
         // split the json-build, the posting, and the resp-build into 3 method calls
         // just for ease of breakpointing
         jsonBytes = avroToJson(dw, reqSchema, theAvroReq);
+
+
         httpResp = jsonPost(urlRoot + path);
+        if (wireDiff != null) wireDiff.setActJson(httpResp.getBody().toString());
+
         theResp = makeAvroFromResponse(httpResp, urlRoot + path);
 
         // track all message types sent/received for simple "test coverage" indication
-        String respName = theResp != null? theResp.getClass().getSimpleName() : "null";
-        messages.put(theAvroReq.getClass().getSimpleName()+" POST <"+jsonBytes+">", respName, httpResp.getStatus());;
+        String respName = theResp != null ? theResp.getClass().getSimpleName() : "null";
+        messages.put(theAvroReq.getClass().getSimpleName() + " POST <" + jsonBytes + ">", respName, httpResp.getStatus());
+
 
         return theResp;
     }
 
+    /**
+     * Perform GET (according the data stored in this object at construction).
+     * <p>
+     * If this object has a WireDiff then the return JSON (if any) is copied into that.
+     * This method also tracks all message types sent and received, in the 'messages' Table. The
+     * 'sent' message type (the Request type) is meaningless, except that it sets the target URL
+     * during object construction - the Request type avro object isn't actually serialized.
+     *
+     * @return an instance of the response type (as set during onbject construction), can be null.
+     */
     public P doGetResp(String id) {
 
         // no request object to build, just GET from the endpoint with route param
-        httpResp = jsonGet(urlRoot+path, id);
+        httpResp = jsonGet(urlRoot + path, id);
+        if (wireDiff != null) wireDiff.setActJson(httpResp.getBody().toString());
+
         theResp = makeAvroFromResponse(httpResp, urlRoot + path + "/" + id);
 
         // track all message types sent/received for simple "test coverage" indication
-        String respName = theResp != null? theResp.getClass().getSimpleName() : "null";
+        String respName = theResp != null ? theResp.getClass().getSimpleName() : "null";
         messages.put(theAvroReq.getClass().getSimpleName() + " GET <" + id + ">", respName, httpResp.getStatus());
 
         return theResp;
     }
 
-    P makeAvroFromResponse(HttpResponse<JsonNode> respToBeConverted, String sourceForLog){
+    P makeAvroFromResponse(HttpResponse<JsonNode> respToBeConverted, String sourceForLog) {
         P response = null;
-        if (httpResp.getStatus() == HttpStatus.SC_OK){
+        if (httpResp.getStatus() == HttpStatus.SC_OK) {
             switch (avroDeserializer) {
                 case JACKSON_AVRO:
                     try {
-                        response = (P)jsonToObject(respToBeConverted.getBody().toString(), theResp.getClass(), theResp.getSchema());
-                    } catch ( JsonMappingException jme){
+                        response = (P) jsonToObject(respToBeConverted.getBody().toString(), theResp.getClass(), theResp.getSchema());
+                    } catch (JsonMappingException jme) {
                         response = null;
                         log.warn("deserializing via " + avroDeserializer + " returns null instead of a " + theResp.getClass().getName()
                                 + " from: " + respToBeConverted.getBody(), jme);
                     }
                     break;
                 case AVRO_DIRECT:
-                    try{
-                        response = (P) jsonToAvroObject(respToBeConverted.getBody().toString(),theResp.getSchema());
-                    } catch ( AvroTypeException ate){
+                    try {
+                        response = (P) jsonToAvroObject(respToBeConverted.getBody().toString(), theResp.getSchema());
+                    } catch (AvroTypeException ate) {
                         response = null;
                         log.warn("deserializing via " + avroDeserializer + " returns null instead of a " + theResp.getClass().getName()
                                 + " from: " + respToBeConverted.getBody(), ate);
                     }
                     break;
                 case JACKSON_RELAXED:
-                    response = (P)jsonToObject(respToBeConverted.getBody().toString(), theResp.getClass());
+                    response = (P) jsonToObject(respToBeConverted.getBody().toString(), theResp.getClass());
                     break;
             }
         } else {
             log.warn("makeAvroFromResponse returns null instead of a " + theResp.getClass().getName()
-                    + " because rcd status " + httpResp.getStatus()
-                    + " with response_BODY < "+ httpResp.getBody() + " >"
-                    + " from " + sourceForLog + "with request_BODY < " + String.valueOf(jsonBytes) + " >"
-                    );
+                            + " because rcd status " + httpResp.getStatus()
+                            + " with response_BODY < " + httpResp.getBody() + " >"
+                            + " from " + sourceForLog + "with request_BODY < " + String.valueOf(jsonBytes) + " >"
+            );
         }
         return response;
     }
@@ -181,7 +240,7 @@ public class AvroJson<Q extends org.apache.avro.generic.GenericContainer,P exten
     }
 
     HttpResponse<JsonNode> jsonPost(String theURL) {
-        if(log.isDebugEnabled()){
+        if (log.isDebugEnabled()) {
             log.debug("begin jsonPost to " + theURL + " of " + String.valueOf(jsonBytes));
         }
         HttpResponse<JsonNode> jsonResponse = null;
@@ -194,14 +253,14 @@ public class AvroJson<Q extends org.apache.avro.generic.GenericContainer,P exten
         } catch (UnirestException e) {
             log.warn("problem communicating JSON with " + theURL, e);
         }
-        if(log.isDebugEnabled()){
+        if (log.isDebugEnabled()) {
             log.debug("exit jsonPost to " + theURL + " with status " + jsonResponse.getStatusText());
         }
         return jsonResponse;
     }
 
-    HttpResponse<JsonNode> jsonGet(String theUrl, String id){
-        if(log.isDebugEnabled()){
+    HttpResponse<JsonNode> jsonGet(String theUrl, String id) {
+        if (log.isDebugEnabled()) {
             log.debug("begin jsonGet to " + theUrl + " id=" + id);
         }
         HttpResponse<JsonNode> jsonResponse = null;
@@ -209,21 +268,25 @@ public class AvroJson<Q extends org.apache.avro.generic.GenericContainer,P exten
         try {
             jsonResponse = Unirest.get(theUrl)
                     .header("accept", "application/json")
-                    .routeParam("id",id)
+                    .routeParam("id", id)
                     .asJson();
         } catch (UnirestException e) {
             log.warn("problem communicating JSON with " + theUrl + " id: " + id, e);
         }
-        if(log.isDebugEnabled()){
+        if (log.isDebugEnabled()) {
             log.debug("exit jsonGet to " + theUrl + " id=" + id + " with status " + jsonResponse.getStatusText());
+        }
+        if (wireDiff != null) {
+            wireDiff.setActJson(jsonResponse.getBody().toString());
         }
         return jsonResponse;
     }
 
-    public void jsonCompare(){
+    public void jsonCompare() {
 
     }
-    Object jsonToObject( String jsonString, Class objClass){
+
+    Object jsonToObject(String jsonString, Class objClass) {
         Object target = null;
 
         try {
@@ -237,10 +300,7 @@ public class AvroJson<Q extends org.apache.avro.generic.GenericContainer,P exten
         return target;
     }
 
-    // UNUSED BELOW: work-in-progress for Avro-specific deserialization
-
-
-    public Object jsonToObject( String jsonString, Class objClass, Schema schema) throws JsonMappingException {
+    public Object jsonToObject(String jsonString, Class objClass, Schema schema) throws JsonMappingException {
 
         AvroSchema avSchema = new AvroSchema(schema);
         ObjectMapper om = new ObjectMapper(new AvroFactory());
@@ -256,11 +316,11 @@ public class AvroJson<Q extends org.apache.avro.generic.GenericContainer,P exten
         return obj;
     }
 
+    // UNUSED BELOW: work-in-progress for Avro-specific deserialization
 
-
-    public <T> T jsonToAvroObject(String theJson, Schema schema){
+    public <T> T jsonToAvroObject(String theJson, Schema schema) {
         byte[] avroByteArray = fromJsonToBytes(theJson, schema);
-        if(avroByteArray == null || avroByteArray.length == 0){
+        if (avroByteArray == null || avroByteArray.length == 0) {
             return null;
         }
 
@@ -270,12 +330,12 @@ public class AvroJson<Q extends org.apache.avro.generic.GenericContainer,P exten
         try {
             result = reader1.read(null, decoder1);
         } catch (IOException e) {
-            log.warn("Avro reader failed to get bytes for Schema " + schema.getName(),e);
+            log.warn("Avro reader failed to get bytes for Schema " + schema.getName(), e);
         }
         return (T) result;
     }
 
-    public byte[] fromJsonToBytes(String theJson, Schema schema){
+    public byte[] fromJsonToBytes(String theJson, Schema schema) {
         ByteArrayOutputStream outputStream = null;
 
         InputStream input = new ByteArrayInputStream(theJson.getBytes());
@@ -295,14 +355,19 @@ public class AvroJson<Q extends org.apache.avro.generic.GenericContainer,P exten
 
         } catch (IOException e) {
             log.warn("IOException getting bytes", e);
+        } catch (org.apache.avro.AvroTypeException te) {
+            log.warn("Failed fromJsonToBytes for " + schema.getName() + " on " + theJson, te);
         }
-        catch (org.apache.avro.AvroTypeException te){
-            log.warn("Failed fromJsonToBytes for " + schema.getName() + " on "+theJson, te);
-        }
-        byte[] outbytes = new byte[] {};
-        if(outputStream != null){
+        byte[] outbytes = new byte[]{};
+        if (outputStream != null) {
             outbytes = outputStream.toByteArray();
         }
         return outbytes;
+    }
+
+    public enum DESER_MODE {
+        JACKSON_RELAXED,
+        JACKSON_AVRO,
+        AVRO_DIRECT
     }
 }
