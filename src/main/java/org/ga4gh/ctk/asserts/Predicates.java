45ab17a4688369410e5a9421f02762091ffd1b2b
Wayne Stidolph <wayne@stidolph.com>
2015-05-29 12:17:32 -0700

    remove demo SimpleOrderService cruft

--- src/main/java/org/ga4gh/ctk/asserts/Predicates.java
+++ src/main/java/org/ga4gh/ctk/asserts/Predicates.java
@@ -1,15 +1,8 @@
 package org.ga4gh.ctk.asserts;
 
-import org.ga4gh.service.Confirmation;
-import org.ga4gh.service.Order;
-
-import java.util.function.BiPredicate;
-
 /**
  * Created by Wayne Stidolph on 5/19/2015.
  */
 public class Predicates {
-    public BiPredicate<Confirmation,Order> customerIdPreserved =
-            (Confirmation C, Order O) -> C.getCustomerId() == O.getCustomerId();
 
 }
