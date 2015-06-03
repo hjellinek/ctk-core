330d070b19c1ffcf14928a501512edfe1adbb2b5
Wayne Stidolph <wayne@stidolph.com>
2015-06-03 12:37:32 -0700

    Basic check the app comes up.

---
+++ src/test/java/org/ga4gh/ctk/ApplicationTest.java
@@ -0,0 +1,33 @@
+package org.ga4gh.ctk;
+
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
+
+/**
+ * Application Tester.
+ *
+ * @author <Authors name>
+ * @version 1.0
+ * @since <pre>May 29, 2015</pre>
+ */
+public class ApplicationTest {
+
+    @Before
+    public void before() throws Exception {
+    }
+
+    @After
+    public void after() throws Exception {
+    }
+
+    /**
+     * Method: main(String[] args)
+     */
+    @Test
+    public void testMain() throws Exception {
+//TODO: Test goes here...
+
+    }
+
+} 
