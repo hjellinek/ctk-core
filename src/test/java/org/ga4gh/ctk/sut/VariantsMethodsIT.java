349b1f0aa9d8df7c20732855a764d7b41ff7366e
Wayne Stidolph <wayne@stidolph.com>
2015-06-06 20:28:25 -0700

    Add empty Variants IT test class.

---
+++ src/test/java/org/ga4gh/ctk/sut/VariantsMethodsIT.java
@@ -0,0 +1,105 @@
+package org.ga4gh.ctk.sut;
+
+import org.junit.Test; 
+import org.junit.Before; 
+import org.junit.After; 
+
+/** 
+* VariantsProtocolClient Tester. 
+* 
+* @author <Authors name> 
+* @since <pre>Jun 6, 2015</pre> 
+* @version 1.0 
+*/ 
+public class VariantsMethodsIT {
+
+@Before
+public void before() throws Exception { 
+} 
+
+@After
+public void after() throws Exception { 
+} 
+
+/** 
+* 
+* Method: searchVariantSets(GASearchVariantSetsRequest request) 
+* 
+*/ 
+@Test
+public void testSearchVariantSetsRequest() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: searchVariantSets(GASearchVariantSetsRequest request, WireDiff wd) 
+* 
+*/ 
+@Test
+public void testSearchVariantSetsForRequestWd() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: searchVariants(GASearchVariantsRequest request) 
+* 
+*/ 
+@Test
+public void testSearchVariantsRequest() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: searchVariants(GASearchVariantsRequest request, WireDiff wd) 
+* 
+*/ 
+@Test
+public void testSearchVariantsForRequestWd() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: searchCallSets(GASearchCallSetsRequest request) 
+* 
+*/ 
+@Test
+public void testSearchCallSetsRequest() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: searchVariantSets(GASearchVariantSetsRequest var1, org.apache.avro.ipc.Callback<GASearchVariantSetsResponse> var2) 
+* 
+*/ 
+@Test
+public void testSearchVariantSetsForGaSearchVariantSetsRequestCallback() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: searchVariants(GASearchVariantsRequest var1, org.apache.avro.ipc.Callback<GASearchVariantsResponse> var2) 
+* 
+*/ 
+@Test
+public void testSearchVariantsForGaSearchVariantsRequestCallback() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: searchCallSets(GASearchCallSetsRequest var1, org.apache.avro.ipc.Callback<GASearchCallSetsResponse> var2) 
+* 
+*/ 
+@Test
+public void testSearchCallSetsForGaSearchCallSetsRequestCallback() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+
+} 
