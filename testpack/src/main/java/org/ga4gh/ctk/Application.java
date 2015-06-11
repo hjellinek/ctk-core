896d4f744e3ac06e94bc316527a851ee32eaabe6
Wayne Stidolph <wayne@stidolph.com>
2015-06-11 13:30:54 -0700

    add reflection to main Application so we can launch test classes from jar on classpath.
    Need a launch string like:
    
    java -classpath ctk-testpack-0.5.1-SNAPSHOT.jar;ctk-testpack-0.5.1-SNAPSHOT-tests.jar org.springframework.boot.loader.JarLauncher
    
    Fixed launch behavior right now, runs all tests in org.ga4gh.ctk.systests.**/* which have a name ending in IT; still have to add categories, and suites - but this is a step forward!

--- testpack/src/main/java/org/ga4gh/ctk/Application.java
+++ testpack/src/main/java/org/ga4gh/ctk/Application.java
@@ -3,16 +3,22 @@ package org.ga4gh.ctk;
 import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
 import org.junit.runner.notification.Failure;
+import org.reflections.Reflections;
+import org.reflections.scanners.SubTypesScanner;
+import org.reflections.util.ClasspathHelper;
+import org.reflections.util.ConfigurationBuilder;
+import org.reflections.util.FilterBuilder;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Set;
 
 @SpringBootApplication
 public class Application implements CommandLineRunner {
+
+    private static String TESTPACKAGE = "org.ga4gh.ctk.systests";
 
     public static void main(String[] args) {
         ApplicationContext ctx = SpringApplication.run(Application.class, args);
@@ -26,24 +32,42 @@ public class Application implements CommandLineRunner {
      */
     @Override
     public void run(String... args) throws Exception {
-        List<Class> testCases = new ArrayList();
+        String matchStrIT = ".*IT.*";
+        String matchStrSuite = ".*TestSuite/..*";
+        Set<Class<?>> testCases = findTestClasses(matchStrIT);
 
-        // we need the app classpath to include the test classes
-        // and for them to e packaged in the app jar
-
-       // testCases.add(ReadMethodsIT.class);
 
         for (Class testCase : testCases) {
-            System.out.println("loop top");
             try {
                 runTestCase(testCase);
             } catch (Throwable t) {
                 t.printStackTrace();
             }
-
-
         }
+    }
 
+    /*
+    -Dsuite=<foo> matches <foo>TestSuite
+    -Dcategory=<foo> matches any org.ga4gh.ctk.systests test annotated with @Test and @<foo>.class
+    -Dtest=<foo> matches org.ga4gh.ctk.systests/<0 or more dirs>/<foo>
+     */
+
+    /**
+     * find test classes (classes in org.ga4gh.ctk.systests/** with name ending in IT
+    */
+    private static Set<Class<?>> findTestClasses(String matchStr) {
+
+        Reflections reflections =
+                new Reflections(new ConfigurationBuilder()
+                .setUrls(ClasspathHelper.forPackage(TESTPACKAGE))
+                .setScanners(new SubTypesScanner(false)) // false means do NOT exclude direct child of Object
+                .filterInputsBy(new FilterBuilder().include(matchStr))
+        );
+
+        Set<Class<? extends Object>> testClasses =
+                reflections.getSubTypesOf(Object.class);
+
+        return testClasses;
     }
 
     private static void runTestCase(Class testCase) {
