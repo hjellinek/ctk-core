c85e2bf0889be31bc04c78b2266ac2b8ec1d3b7d
Wayne Stidolph <wayne@stidolph.com>
2015-06-15 17:23:15 -0700

    Use the Wildcard Runner.

--- testpack/src/test/java/org/ga4gh/ctk/systests/CoreTestSuite.java
+++ testpack/src/test/java/org/ga4gh/ctk/systests/CoreTestSuite.java
@@ -1,14 +1,17 @@
 package org.ga4gh.ctk.systests;
 
+import com.googlecode.junittoolbox.IncludeCategories;
+import com.googlecode.junittoolbox.SuiteClasses;
+import com.googlecode.junittoolbox.WildcardPatternSuite;
 import org.ga4gh.ctk.control.testcategories.CoreTests;
-import org.junit.experimental.categories.Categories;
 import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
 
 /**
  * Created by Wayne Stidolph on 6/7/2015.
  */
-@RunWith(Categories.class)
-@Suite.SuiteClasses(CoreTests.class)
+
+@RunWith(WildcardPatternSuite.class)
+@IncludeCategories(CoreTests.class)
+@SuiteClasses({"**/*IT.class", "**/*Test.class"})
 public class CoreTestSuite {
 }
