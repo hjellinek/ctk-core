3dfc3afc3eda72e6efe5dac85bda36ed5699300b
Wayne Stidolph <wayne@stidolph.com>
2015-06-15 20:06:45 -0700

    Implement basic fetch/check-for-200.

--- testpack/src/test/java/org/ga4gh/ctk/systests/core/LandingPageIT.java
+++ testpack/src/test/java/org/ga4gh/ctk/systests/core/LandingPageIT.java
@@ -6,25 +6,44 @@ package org.ga4gh.ctk.systests.core;
  * <p>Created by Wayne Stidolph on 6/7/2015.</p>
  */
 
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.request.GetRequest;
 import junitparams.JUnitParamsRunner;
 import org.ga4gh.ctk.control.testcategories.CoreTests;
-import org.ga4gh.ctk.control.testcategories.WIP;
-import org.junit.Ignore;
+import org.ga4gh.ctk.transport.URLMAPPING;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
 
+import java.net.URL;
+
+import static org.slf4j.LoggerFactory.getLogger;
+
 @Category(CoreTests.class)
 @RunWith(JUnitParamsRunner.class)
 public class LandingPageIT {
+    private static org.slf4j.Logger log = getLogger(LandingPageIT.class);
 
+    /**
+     * <p>Landing pages should exist.</p>
+     * <p>This test verifies the environmental "urlRoot" is a valid URL
+     * and then tries to fetch the page at that root. A response of '200'
+     * is adequate for passing the test.</p>
+     *
+     * @throws Exception the exception
+     */
     @Test
-    @Category(WIP.class)
-    @Ignore("Placeholder, not yet written")
     public static void landingPagesShouldExist() throws Exception {
-        // TODO fetch page object and verify OK
+        String theUrlString = URLMAPPING.getUrlRoot();
 
-        // TODO verify correct data installed (scrape landing page)
+        URL url = new URL(theUrlString);
+        String finalUrl = url.getProtocol() + "://" + url.getAuthority();
 
+        log.debug("Fetch from " + finalUrl);
+        GetRequest request = Unirest.get(finalUrl);
+        HttpResponse<String> response = request.asString();
+        org.assertj.core.api.Assertions.assertThat(response.getStatus()).isEqualTo(200);
     }
+
 }
