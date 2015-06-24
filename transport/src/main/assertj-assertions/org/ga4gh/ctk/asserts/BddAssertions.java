f1479fa2adee1d5f46d3bd557995571d6fdff187
Wayne Stidolph <wayne@stidolph.com>
2015-06-24 09:39:06 -0700

    reset schemas to v0.5.2 tag and regen asserts (error is from getting out of sync in submodule while swithcing branches in main module)

--- transport/src/main/assertj-assertions/org/ga4gh/ctk/asserts/BddAssertions.java
+++ transport/src/main/assertj-assertions/org/ga4gh/ctk/asserts/BddAssertions.java
@@ -597,26 +597,6 @@ public class BddAssertions {
   }
 
   /**
-   * Creates a new instance of <code>{@link org.ga4gh.ctk.transport.RespCodeAssert}</code>.
-   *
-   * @param actual the actual value.
-   * @return the created assertion object.
-   */
-  public static org.ga4gh.ctk.transport.RespCodeAssert then(org.ga4gh.ctk.transport.RespCode actual) {
-    return new org.ga4gh.ctk.transport.RespCodeAssert(actual);
-  }
-
-  /**
-   * Creates a new instance of <code>{@link org.ga4gh.ctk.transport.WireTrackerAssert}</code>.
-   *
-   * @param actual the actual value.
-   * @return the created assertion object.
-   */
-  public static org.ga4gh.ctk.transport.WireTrackerAssert then(org.ga4gh.ctk.transport.WireTracker actual) {
-    return new org.ga4gh.ctk.transport.WireTrackerAssert(actual);
-  }
-
-  /**
    * Creates a new <code>{@link BddAssertions}</code>.
    */
   protected BddAssertions() {
